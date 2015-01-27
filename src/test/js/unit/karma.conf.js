87ea3ea1b0fd541a8f44fadebdbb36047210c13a
bgawel <bartek.gawel@gmail.com>
2015-01-27 22:23:24 +0100

    added karma phantomjs launcher

--- src/test/js/unit/karma.conf.js
+++ src/test/js/unit/karma.conf.js
@@ -43,7 +43,7 @@ module.exports = function(config) {
     // - Safari (only Mac)
     // - PhantomJS
     // - IE (only Windows)
-    browsers: ['Chrome', 'Firefox'],
+    browsers: ['Chrome', 'Firefox', 'PhantomJS'],
 
 
     // Continuous Integration mode
